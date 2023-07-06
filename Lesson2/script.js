a54ae7dbd7521f48dac591d6f4cd7c4f44823ee1
JabbarJabbarli <cabbarcabbarli2005@gmail.com>
2023-07-06 12:08:08 +0400

    first commit

---
+++ Lesson2/script.js
@@ -0,0 +1,148 @@
+// console.log(true)
+// console.log(typeof("Jabbar"));
+// console.log(typeof(5.2))
+// console.log("false")
+// console.log(typeof(true));
+
+
+
+
+
+
+
+
+
+
+
+// camelCase
+// let mackbookPrice = 3200
+
+// snakelCase
+// let macbook_Price = 3201
+
+//PascalCase
+// let MackbookPrice = 3202
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arithmetic operator
+
+//addition
+// let a = 10, b = 329
+// let result1 = a + b
+// console.log(`${a}+${b}=${result1}`)
+
+// //subtraction
+// let result2 = a - b 
+// console.log(`${a}-${b}=${result2}`)
+
+// //multiplification
+
+// let result3 = a*b 
+// console.log(`${a}*${b}=${result3}`)
+
+
+// //division
+// let result4 = b/a
+// console.log(`${a}/${b}=${result4}`)
+
+// //mod %
+// let result5= b%a
+// console.log(`${a}+${b}=${result5}`)
+
+// //exponentiation
+// let result6= b**a
+// console.log(`${a}**${b}=${result6}`)
+
+
+
+// // /**
+//  let rogPrice = 3200
+ 
+//  if(rogPrice=3200){
+//      for (let rogPrice = 0; rogPrice < 3200; rogPrice++) {
+         
+//          console.log(rogPrice)
+//      }
+//  }
+ 
+
+//  * */
+
+
+
+
+
+//  let name= "Jabbar", age= 18
+
+//  console.log(`Hi my name is ${name}, I am ${age} years old`)     
+
+
+
+
+
+
+
+
+
+//Asignment
+
+// let a = 22, b= 3
+
+
+// console.log(a+=b);
+// console.log(`a: ${a}`);
+// console.log(`b: ${b}`);
+// a+=b
+// console.log("a+=b");
+// console.log(`a: ${a}`);
+// console.log(`b: ${b}`);
+
+// console.log(`a:${a}, ${b}`);
+
+// a%=b
+
+// console.log(`a%=b`);
+// console.log(`a:${a} b:${b}`);
+
+
+
+
+
+
+
+//comparison operator
+
+
+// let a = 5, b = 10
+
+// a==5 
+// a===5
+
+// console.log(5 != 10);
+// console.log("10" !== 10);
+
+// console.log(`${a} < ${b} = ${a < b}`);
+// console.log(`${a} > ${b} = ${a > b}`);
+
+
+
+
+// let a = 5 , b = 10
+
+// a<=b
+
